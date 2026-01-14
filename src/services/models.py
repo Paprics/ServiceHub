803a2c749b92e5cee804a06edcd4be2b1c78b4ef
Valeriy D. <paprics@gmail.com>
2026-01-14 15:06:19 +0000

    add minimal base model

--- src/services/models.py
+++ src/services/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=250)
+    description = models.TextField(max_length=5000)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='products',
+        blank=False,
+    )
+
+    def __str__(self):
+        return f'{self.title} | {self.category.name}'
