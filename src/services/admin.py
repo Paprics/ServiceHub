165a65be9b85cd84d259419377a2c9ca574894df
Valeriy D. <paprics@gmail.com>
2026-01-15 10:11:25 +0000

    Add test data. Script (fixtures, create user)

--- src/services/admin.py
+++ src/services/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Package, FAQ, AdditionalService
 
 
 admin.site.register(Product)
 
 admin.site.register(Category)
+
+admin.site.register(Package)
+
+admin.site.register(FAQ)
+
+admin.site.register(AdditionalService)
--- src/services/models.py
+++ src/services/models.py
@@ -20,3 +20,45 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.title} | {self.category.name}'
+
+
+class Package(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='packages'
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return f'{self.name} ({self.product.title})'
+
+
+class AdditionalService(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='additional_services'
+    )
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return f'{self.name} ({self.product.title})'
+
+
+class FAQ(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='faqs'
+    )
+    question = models.CharField(max_length=500)
+    answer = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return f'Q: {self.question} | {self.product.title}'
