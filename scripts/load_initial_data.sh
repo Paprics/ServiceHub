4a12bc525d6d771d79658020548fbca7796690d7
Valeriy D. <paprics@gmail.com>
2026-01-16 14:57:11 +0000

    Add REDME file

--- scripts/load_initial_data.sh
+++ scripts/load_initial_data.sh
@@ -14,7 +14,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 USERNAME = "admin"
 EMAIL = "admin@example.com"
-PASSWORD = "1234"
+PASSWORD = "admin"
 
 if not User.objects.filter(username=USERNAME).exists():
     User.objects.create_superuser(
